e63c7843405a1d85429938161f83201631282398
Pablo Calvo Sastre <>
2019-11-08 13:49:59 +0100

    refactor ResumeControllerTest

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/ResumeControllerTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/ResumeControllerTest.java
@@ -4,16 +4,26 @@ import static org.junit.Assert.assertEquals;
 
 import es.urjccode.mastercloudapps.adcs.draughts.models.Game;
 import es.urjccode.mastercloudapps.adcs.draughts.models.State;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import es.urjccode.mastercloudapps.adcs.draughts.models.StateValue;
 
 public class ResumeControllerTest {
 
+    private Game game;
+    private State state;
+
+    @Before
+    public void initTest(){
+        this.game = new Game();
+        this.state = new State();
+    }
+
     @Test
     public void givenResumeControllerWhenResumeGameMoveToInitialStateRequiereCorrectThenNotError() {
-        Game game = new Game();
-        State state = new State();
+
         ResumeController resumeController = new ResumeController(game, state);
         assertEquals(StateValue.INITIAL, state.getValueState());
         resumeController.next();
@@ -26,8 +36,7 @@ public class ResumeControllerTest {
 
     @Test(expected = AssertionError.class)
     public void givenResumeControllerWhenResumeGameMoveOutThenError() {
-        Game game = new Game();
-        State state = new State();
+
         ResumeController resumeController = new ResumeController(game, state);
         assertEquals(StateValue.INITIAL, state.getValueState());
         resumeController.next();
