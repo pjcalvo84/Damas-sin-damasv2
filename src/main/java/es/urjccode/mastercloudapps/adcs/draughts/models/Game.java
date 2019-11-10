94a039d6b1225c0d279b62e9ce9d58f195c401a3
Pablo Calvo <babel.pcalvo@iberia.es>
2019-11-10 16:42:37 +0100

    extract check move to new method

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Game.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Game.java
@@ -38,42 +38,50 @@ public class Game {
 
 	public Error move(Coordinate origin, Coordinate target) {
 		assert origin != null && target != null;
-		if (!origin.isValid() || !target.isValid()) {
-			return Error.OUT_COORDINATE;
-		}
-		if (board.isEmpty(origin)) {
-			return Error.EMPTY_ORIGIN;
-		}
-		Color color = this.board.getColor(origin);
-		if (this.turn.getColor() != color) {
-			return Error.OPPOSITE_PIECE;
-		}
-		if (!origin.isDiagonal(target)) {
-			return Error.NOT_DIAGONAL;
-		}
-		Piece piece = this.board.getPiece(origin);
-		if (!piece.isAdvanced(origin, target)) {
-			return Error.NOT_ADVANCED;
-		}
-		if (origin.diagonalDistance(target) >= 3) {
-			return Error.BAD_DISTANCE;
-		}
-		if (!this.board.isEmpty(target)) {
-			return Error.NOT_EMPTY_TARGET;
-		}
-		if (origin.diagonalDistance(target) == 2) {
-			Coordinate between = origin.betweenDiagonal(target);
-			if (this.board.getPiece(between) == null) {
-				return Error.EATING_EMPTY;
-			}
-			this.board.remove(between);
-		}
+        final Error x = checkMovePeon(origin, target);
+        if(x != null)
+            return x;
+
 		this.board.move(origin, target);
 		this.turn.change();
 		return null;
 	}
 
-	public Color getColor(Coordinate coordinate) {
+    private Error checkMovePeon(Coordinate origin, Coordinate target){
+        if (!origin.isValid() || !target.isValid()) {
+            return Error.OUT_COORDINATE;
+        }
+        if (board.isEmpty(origin)) {
+            return Error.EMPTY_ORIGIN;
+        }
+        Color color = this.board.getColor(origin);
+        if (this.turn.getColor() != color) {
+            return Error.OPPOSITE_PIECE;
+        }
+        if (!origin.isDiagonal(target)) {
+            return Error.NOT_DIAGONAL;
+        }
+        Piece piece = this.board.getPiece(origin);
+        if (!piece.isAdvanced(origin, target)) {
+            return Error.NOT_ADVANCED;
+        }
+        if (origin.diagonalDistance(target) >= 3) {
+            return Error.BAD_DISTANCE;
+        }
+        if (!this.board.isEmpty(target)) {
+            return Error.NOT_EMPTY_TARGET;
+        }
+        if (origin.diagonalDistance(target) == 2) {
+            Coordinate between = origin.betweenDiagonal(target);
+            if (this.board.getPiece(between) == null) {
+                return Error.EATING_EMPTY;
+            }
+            this.board.remove(between);
+        }
+        return null;
+    }
+
+    public Color getColor(Coordinate coordinate) {
 		return this.board.getColor(coordinate);
 	}
 
